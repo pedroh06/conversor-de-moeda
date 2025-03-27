f446e16a75eb24d7b003243b399339e412e41a7c
pedroh06 <pedrohcsilva25@gmail.com>
2025-03-27 12:17:55 -0300

    correção no js e html

---
+++ main.js
@@ -0,0 +1,15 @@
+function converter() {
+    const real = parseFloat(document.getElementById('real').value);
+
+    if (isNaN(real) || real <= 0) {
+        alert('Insira um valor válido em Reais.');
+        return;
+    }
+
+    const cotacao = 5.17; // cotação fixa, futuramente trocar por uma api
+    const dolar = real / cotacao;
+
+    document.getElementById('dolar').textContent = `Valor em dólares: $${dolar.toFixed(2)}`;
+
+    console.log("Conversão realizada!");
+}
